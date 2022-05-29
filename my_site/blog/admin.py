e27ab685badec37eac498d03da03e1b6afc2c5bf
the-n3rdman <ashishanand2609@gmail.com>
2022-05-29 19:42:48 +0530

    added models

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author',)
+
+
+admin.site.register(models.Author)
+admin.site.register(models.Tag)
+admin.site.register(models.Post, PostAdmin)
