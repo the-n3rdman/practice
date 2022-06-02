b637f4466286071ea39ca19476e0317ee431d8f3
the-n3rdman <ashishanand2609@gmail.com>
2022-06-03 00:19:28 +0530

    Added concept of ModelForm

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,10 +1,26 @@
 from django import forms
+from . import models
+
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(label='Your name', max_length=100, error_messages={
+#         "required": "Name field cant be empty",
+#         "max_length": "please enter a shorter name"
+#     })
+#     review_text = forms.CharField(label='Your feedback', widget=forms.Textarea)
+#     rating = forms.IntegerField(label='Your rating', min_value=1, max_value=5)
 
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(label='Your name', max_length=100, error_messages={
-        "required": "Name field cant be empty",
-        "max_length": "please enter a shorter name"
-    })
-    review_text = forms.CharField(label='Your feedback', widget=forms.Textarea)
-    rating = forms.IntegerField(label='Your rating', min_value=1, max_value=5)
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = models.Review
+        fields = '__all__'
+        labels = {
+            'user_name': 'Your Name',
+            'review_text': 'Review'
+        }
+        error_messages = {
+            'user_name': {
+                "required": "Name field cant be empty",
+                "max_length": "please enter a shorter name"
+            }
+        }
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -20,11 +20,21 @@ def review(request):
     # using Form class from forms.py
 
     if request.method == 'POST':
+        # To update an already existing data entry in our database, we just need to get that instance
+        # and then pass that instance as the second parameter here when using ModelForms
+        # For ex:
+        # existing_review = Review.objects.get(pk = 1)
+        # form = forms.ReviewForm(request.POST, instance = existing_review)
+
         form = forms.ReviewForm(request.POST)
         if form.is_valid():
-            review = models.Review(user_name=form.cleaned_data['user_name'],
-                                   review_text=form.cleaned_data['review_text'],
-                                   rating=form.cleaned_data['rating'])
+            # review = models.Review(user_name=form.cleaned_data['user_name'],
+            #                        review_text=form.cleaned_data['review_text'],
+            #                        rating=form.cleaned_data['rating'])
+
+            # If we are using Regular forms then we have to save the data into model like above
+            # But if our ReviewForm is a Model Form then we can directly call form.save after validating
+            form.save()
             return HttpResponseRedirect('/thank_you')
     else:
         # adding an else here so if the request method is get then only new form is shown but if get method is post and form is invalid then the form with errors is renderd
